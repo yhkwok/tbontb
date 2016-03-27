aa910d802f213faf44be75fd75cb86c9fbde469b
yhkwok <kwo13001@byui.edu>
2016-03-26 23:14:12 -0600

    edited AddPollToDB

--- ToBuyOrNotToBuy/src/main/java/AddPollToDB.java
+++ ToBuyOrNotToBuy/src/main/java/AddPollToDB.java
@@ -6,6 +6,10 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,9 +31,11 @@ public class AddPollToDB extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     * @throws java.lang.ClassNotFoundException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException, ClassNotFoundException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             
@@ -44,18 +50,67 @@ public class AddPollToDB extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             
-            out.println("ADD ME TO DB(PollName): " + selectedItems.getPollName() + "</br>");
-            for (Product p : selectedItems.getProducts())
-            {
-                out.println("<div>");
-                out.println("ADD ME TO DB(Name): " + p.getName() + "</br>");
-                out.println("ADD ME TO DB(Cost): " + p.getCost()+ "</br>");
-                out.println("ADD ME TO DB(Desc): " + p.getDescription()+ "</br>");
-                out.println("ADD ME TO DB(ImgUrl): " + p.getImageURL()+ "</br>");
-                out.println("ADD ME TO DB(BuyUrl) " + p.getBuyURL()+ "</br>");
-                out.println("</div><br/><br/>");
-                //comment
-            }
+            String URL = "jdbc:mysql://localhost/tbontb"; //<--change database name
+            String USER = "root";
+            String PASS = "";
+            Connection conn = null;
+            Statement stmt = null;
+            try {
+                //STEP 2: Register JDBC driver
+                Class.forName("com.mysql.jdbc.Driver");
+
+                //STEP 3: Open a connection
+                System.out.println("Connecting to a selected database...");
+                conn = DriverManager.getConnection(URL, USER, PASS);
+                System.out.println("Connected database successfully...");
+                
+                 //STEP 4: Execute a query
+                System.out.println("Creating statement...");
+                stmt = conn.createStatement();
+                
+                out.println("ADD ME TO DB(PollName): " + selectedItems.getPollName() + "</br>");
+
+                String sql = "Insert INTO UserPolls(creatorUserID, pollName) VALUES "
+                        + "(1, " + selectedItems.getPollName() + ")";
+                //execute and get last insert id
+                int pollID = stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+                
+                
+                
+                for (Product p : selectedItems.getProducts())
+                {
+                    out.println("<div>");
+                    out.println("ADD ME TO DB(Name): " + p.getName() + "</br>");
+                    out.println("ADD ME TO DB(Cost): " + p.getCost()+ "</br>");
+                    out.println("ADD ME TO DB(Desc): " + p.getDescription()+ "</br>");
+                    out.println("ADD ME TO DB(ImgUrl): " + p.getImageURL()+ "</br>");
+                    out.println("ADD ME TO DB(BuyUrl) " + p.getBuyURL()+ "</br>");
+                    out.println("</div><br/><br/>");
+                    //comment
+                
+                     sql = "Insert INTO UserPollItems(name, userPollID, price, description, imageLink, buyLink) VALUES "
+                            + "(" + p.getName() + ", " + pollID + ", " + 
+                             p.getCost() + ", " + p.getDescription() + ", " + 
+                             p.getImageURL() + ", " + p.getBuyURL() + ")";
+                    stmt.executeQuery(sql);
+                }
+            } catch (SQLException | ClassNotFoundException se) {
+            } finally {
+                //finally block used to close resources
+                try {
+                    if (stmt != null) {
+                        conn.close();
+                    }
+                } catch (SQLException se) {
+                }// do nothing
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                } catch (SQLException se) {
+                }//end finally try
+            }//end try
+            
             out.println("</body>");
             out.println("</html>");
         }
